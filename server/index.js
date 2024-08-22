78fc02d88174d19810c310864293000f29c52758
Virendra Kumar <100487078+Virendra-94@users.noreply.github.com>
2024-08-22 14:04:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Use CORS and specify the frontend origin
 app.use(cors({
-  origin: ["https://drop-link-delta.vercel.app"],
+  origin: ["https://drop-link-client.vercel.app"],
   methods: ["POST", "GET"],
   credentials: true
 }
